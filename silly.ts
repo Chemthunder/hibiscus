f1b9160f5722fcf46d497218b69a88c24e596b6b
Chemm :3 <chemthunderisme@gmail.com>
2026-02-13 10:37:10 -0500

    added item system

--- silly.ts
+++ silly.ts
@@ -29,55 +29,67 @@ namespace gameAssets {
         f f f f f f
     `;
     export let emptyImage = img`
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . .
+    `;
+    export let lImage = img`
+        1 1 1 1
+        1 1 1 1
+        1 1 1 1
+        1 1 1 1
+    `;
+    export let card = img`
+        11111111111111
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        1ffffffffffff1
+        11111111111111
     `;
 }
 
 namespace gameUtils {
     export let facing = "";
-    export let parsedPlayer: Sprite;
     export let canInput = true;
     export let hx = 0;
     export let hy = 0;
-    export let shakeIntensity = 0;
-
-    export enum Events {
-        ATTACK
-    }
+    export let isPaused = false;
 
     export function clearMenu() {
         for (let value of menu.menuSprites) {
             sprites.destroy(value);
-            if (hyacinth.isDevelopmentEnvironment(true)) console.log(value.id + " was cleared successfully!");
+         //   if (hyacinth.isDevelopmentEnvironment(true)) console.log(value.id + " was cleared successfully!");
         }
-    }
-
-    export function parsePlayer() {
-        let finalValue;
-        for (let c of sprites.allOfKind(SpriteKind.Player)) {
-            finalValue = c;
-        }
-
-        parsedPlayer = finalValue;
-    }
-
-    export function input(event: Events, target: Sprite) {
-        
     }
     
     forever(function () {
@@ -97,20 +109,20 @@ namespace gameUtils {
             gameUtils.facing = "up";
         });
     });
-
-    forever( function () {
-        scene.cameraShake(shakeIntensity, 40);
-        info.setScore(shakeIntensity);
-    });
 }
 
 namespace inventory {
     let heldItem: Items = null;
     let readout: Sprite = sprites.create(gameAssets.emptyImage, SpriteKind.VisualEffect);
+    let readoutCard: Sprite = sprites.create(gameAssets.card, SpriteKind.VisualEffect);
+
+
     readout.z = -3;
 
     export enum Items {
-        TEST
+        TEST,
+        MAGI,
+        SWRD
     }
 
     export function getItem(): Items {
@@ -126,17 +138,132 @@ namespace inventory {
 
         switch(heldItem) {
             case (Items.TEST): {
-                result = "test";
+                result = "tests";
+                break;
+            }
+
+            case (Items.MAGI): {
+                result = "magic";
+                break;
+            }
+
+            case (Items.SWRD): {
+                result = "sword";
+                break;
             }
         }
 
         return result;
     }
 
+    export function iterateItems(): void {
+        let result: Items;
+
+        switch (heldItem) {
+            case (Items.TEST): {
+                result = Items.MAGI;
+                break;
+            }
+
+            case (Items.MAGI): {
+                result = Items.SWRD;
+                break;
+            }
+
+            case (Items.SWRD): {
+                result = Items.TEST;
+                break;
+            }
+        }
+
+        heldItem = result;
+        scene.cameraShake(1.5, 200);
+    }
+
+    export function useItem(item: Items, sprite: Sprite): void {
+        switch(item) {
+            case (Items.TEST): {
+
+                break;
+            }
+
+            case (Items.MAGI): {
+                let shockwave = sprites.create(img`
+                    .....222222222.....
+                    ...22.........22...
+                    ..2.............2..
+                    .2...............2.
+                    .2...............2.
+                    2.................2
+                    2.................2
+                    2.................2
+                    2.................2
+                    2.................2
+                    2.................2
+                    2.................2
+                    2.................2
+                    2.................2
+                    .2...............2.
+                    .2...............2.
+                    ..2.............2..
+                    ...22.........22...
+                    .....222222222.....
+                `, SpriteKind.Projectile);
+                shockwave.setPosition(sprite.x, sprite.y);
+
+                shockwave.lifespan = 120;
+
+                break;
+            }
+
+            case (Items.SWRD): {
+                
+                break;
+            }
+        }
+
+        console.log("Used: " + parseItemToString(item));
+    }
+
     forever( function () {
-        readout.setPosition(30, 30);
-        readout.sayText(parseItemToString(getItem()), Infinity, false, 15, 1);
+        readout.setPosition(145, 40);
+        readout.sayText(parseItemToString(getItem()), Infinity, false, 1, 15);
+
+        readoutCard.setPosition(readout.x, readout.y + 50);
     });
+}
+
+namespace levels {
+    let currentLevel = 0;
+
+    export function throwLevel(lvl: number, sprite: Sprite) {
+        levelCaches.getAndSetLevel(lvl);
+
+        let x;
+        let y;
+
+        switch(lvl) {
+            case (0): {
+                x = 4;
+                y = 4;
+            }
+        }
+
+        tiles.placeOnTile(sprite, tiles.getTileLocation(x, y));
+    }
+
+    export function getLevel(): number {
+        return currentLevel;
+    }
+
+    namespace levelCaches {
+        export function getAndSetLevel(lvl:number) {
+            if (lvl == 0) {
+              tiles.setCurrentTilemap(tilemap`level1`);
+              // 4, 4
+            }
+        }
+    }
 }
 
 namespace gameScript {
@@ -149,7 +276,7 @@ namespace gameScript {
 
         color.pauseUntilFadeDone();
         gameUtils.clearMenu();
-        scene.setBackgroundColor(1);
+        scene.setBackgroundColor(15);
 
         pause(1000);
         color.startFade(color.Black, color.originalPalette, 1000);
@@ -160,16 +287,21 @@ namespace gameScript {
         player.setPosition(hyacinth.centerScreenX, hyacinth.centerScreenY);
         gameUtils.canInput = true;
         inventory.setItem(inventory.Items.TEST);
-        scene.cameraFollowSprite(player);
 
+        levels.throwLevel(0, player);
+        
         forever(function () {
-//            player.sayText(inventory.parseItemToString(inventory.getItem()));
-
             controller.moveSprite(player, gameUtils.hx, gameUtils.hy);
+
+            controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+                if (gameUtils.canInput) {
+                    inventory.iterateItems();
+                }
+            });
 
             controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
                 if (gameUtils.canInput) {
-                    gameUtils.input(gameUtils.Events.ATTACK, player);
+                    inventory.useItem(inventory.getItem(), player);
                 }
             });
         });
